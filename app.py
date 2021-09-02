c6c08c5428e4fd3c1af853adb389b24b1e60213d
Kaustubh Agrawal <kaustubhagrawal8838@gmail.com>
2021-09-02 18:59:52 +0530

    basic login

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 app = Flask(__name__)
 
 # this is some dummy data for development pourpouse
@@ -14,6 +14,16 @@ products=products = [
 @app.route('/')
 def index():
     return render_template('index.html', items=products)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method== 'GET':
+        return render_template('login-page.html')
+    else:
+        username = request.form['username']
+        password = request.form['password']
+        print(username, ' -> ', password)
+        return f'{username} is logged in'
 
 if __name__ == '__main__':
     app.run(debug=True)
